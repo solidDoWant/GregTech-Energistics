94b5250c47290af5dcd1eac626639ccd57a0db7b
solidDoWant <fred.heinecke@yahoo.com>
2025-12-29 18:50:31 -0600

    Fix dep issues for `runClient`, `runServer`
    Signed-off-by: solidDoWant <fred.heinecke@yahoo.com>

--- src/main/java/com/soliddowant/gregtechenergistics/GregTechEnergisticsMod.java
+++ src/main/java/com/soliddowant/gregtechenergistics/GregTechEnergisticsMod.java
@@ -6,19 +6,17 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = GregTechEnergisticsMod.MODID, name = GregTechEnergisticsMod.NAME, version = GregTechEnergisticsMod.VERSION,
-		dependencies = "required-after:gregtech;required-after:appliedenergistics2;after:jei@[4.15.0,);after:JustEnoughEnergistics")
+@Mod(modid = GregTechEnergisticsMod.MODID, name = GregTechEnergisticsMod.NAME, version = GregTechEnergisticsMod.VERSION, dependencies = "required-after:gregtech;required-after:appliedenergistics2;after:jei@[4.15.0,);after:justenoughenergistics")
 public class GregTechEnergisticsMod {
 	public static final String MODID = "gregtechenergistics";
 	public static final String NAME = "GregTech Energistics";
 	public static final String VERSION = "1.0.0.3";
 
-	@SidedProxy(modId = GregTechEnergisticsMod.MODID, clientSide = "com.soliddowant.gregtechenergistics.ClientProxy",
-			serverSide = "com.soliddowant.gregtechenergistics.CommonProxy")
+	@SidedProxy(modId = GregTechEnergisticsMod.MODID, clientSide = "com.soliddowant.gregtechenergistics.ClientProxy", serverSide = "com.soliddowant.gregtechenergistics.CommonProxy")
 	public static CommonProxy proxy;
-	
+
 	@Mod.Instance
-    public static GregTechEnergisticsMod instance;
+	public static GregTechEnergisticsMod instance;
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
