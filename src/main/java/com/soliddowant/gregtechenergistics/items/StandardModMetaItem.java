7bf0d0f9676a653b312fc862b51f6c21a5101afc
solidDoWant <fred.heinecke@yahoo.com>
2026-01-13 06:10:08 +0000

    Show fluid name, quantity in encoder name (useful for pattern previews)
    Signed-off-by: solidDoWant <fred.heinecke@yahoo.com>

--- src/main/java/com/soliddowant/gregtechenergistics/items/StandardModMetaItem.java
+++ src/main/java/com/soliddowant/gregtechenergistics/items/StandardModMetaItem.java
@@ -1,5 +1,9 @@
 package com.soliddowant.gregtechenergistics.items;
 
+import com.soliddowant.gregtechenergistics.items.behaviors.FluidEncoderBehaviour;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+
 public class StandardModMetaItem extends ModMetaItem<ModMetaItem<?>.ModMetaValueItem> {
     public StandardModMetaItem(short metaItemOffset) {
         super(metaItemOffset);
@@ -8,5 +12,20 @@ public class StandardModMetaItem extends ModMetaItem<ModMetaItem<?>.ModMetaValue
     @Override
     protected ModMetaValueItem constructMetaValueItem(short metaValue, String unlocalizedName) {
         return new ModMetaValueItem(metaValue, unlocalizedName);
+    }
+
+    @Override
+    public String getItemStackDisplayName(ItemStack stack) {
+        String baseName = super.getItemStackDisplayName(stack);
+
+        if (!FluidEncoderBehaviour.hasStackBehavior(stack))
+            return baseName;
+
+        FluidStack fluidStack = FluidEncoderBehaviour.getFluidStack(stack);
+        if (fluidStack == null)
+            return baseName;
+
+        int amount = FluidEncoderBehaviour.getFluidAmount(stack);
+        return baseName + ": " + fluidStack.getLocalizedName() + " (" + amount + " mb)";
     }
 }
